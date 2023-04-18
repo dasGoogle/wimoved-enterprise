5480c3bbd8fccb4d34df68e86ead1d6a68122ed1
Aaron Schlitt <aaron@aaronschlitt.de>
2023-04-18 13:37:57 +0200

    Add debug statement

--- src/Station.cpp
+++ src/Station.cpp
@@ -5,12 +5,14 @@
 #include "Configuration.h"
 #include "MacAddress.h"
 #include "ipc/Caller.h"
+#include "../vendor/logging/loginit.h"
 
 Station::Station(std::string sockname, MacAddress mac)
     : vlan_id(std::nullopt), mac(std::move(mac)), sockname(std::move(sockname)) {}
 
 uint32_t Station::vni() const {
     // Use username as VNI
+    WMLOG(DEBUG) << "Reading VNI " << std::stol(user) <<  " from username " << user;
     return std::stol(user);
 }
 
